dfd62da44000156447b817c292fbf70b7c59758a
FelipeBarros10 <barrosfelipe735@gmail.com>
2024-07-27 15:20:09 -0300

    Including the abou me component

---
+++ src/components/aboutMeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full header-bg-dark">
+    <div class="w-full flex flex-col justify-center items-center">
+      <h3 class="text-purple-700 text-3xl mt-10">Quero me apresentar,</h3>
+      <p class="text-gray-300 mx-14 mt-3 mb-5">
+        Meu nome é Felipe Ferreira de Barros, tenho 22 anos e atualmente estou cursando faculdade.
+        Desde cedo, sempre fui fascinado pelo mundo da tecnologia e, com o tempo, essa paixão me
+        levou a explorar o desenvolvimento front-end. A combinação de lógica, criatividade e a
+        capacidade de criar interfaces intuitivas e visualmente agradáveis me cativou e me fez
+        seguir esse caminho.</p>
+
+      <p class="text-gray-300 mx-14">
+        Este portfólio foi criado com o intuito de mostrar parte das minhas habilidades
+        como desenvolvedor front-end. Aqui trago alguns outros projetos que desenvolvi em HTML, CSS,
+        JavaScript e frameworks modernos como Vue.js e Tailwind.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home/indexPage.vue
+++ src/views/Home/indexPage.vue
@@ -1,16 +1,18 @@
 <template>
   <index-header/>
   <educations-component/>
+  <about-me-component/>
   <!-- <experiencies-component/> -->
 </template>
 
 <script>
 import indexHeader from '@/components/headerComponent.vue'
 import EducationsComponent from '@/components/educationsComponent.vue'
+import aboutMeComponent from '@/components/aboutMeComponent.vue'
 // import experienciesComponent from '@/components/experienciesComponent.vue'
 
 export default {
-  components: { indexHeader, EducationsComponent }
+  components: { indexHeader, EducationsComponent, aboutMeComponent }
 
 }
 </script>
