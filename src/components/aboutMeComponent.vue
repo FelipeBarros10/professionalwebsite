ccc16b791d45b76d3321c119f2b3d26fead25d7d
FelipeBarros10 <barrosfelipe735@gmail.com>
2024-08-01 15:43:21 -0300

    Including the photo in abot component

--- src/components/aboutMeComponent.vue
+++ src/components/aboutMeComponent.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="w-full header-bg-dark">
-    <div class="w-full flex flex-col justify-center items-center">
-      <h3 class="text-purple-700 text-3xl mt-10">Quero me apresentar,</h3>
-      <p class="text-gray-300 mx-14 mt-3 mb-5">
+  <div class="w-full xl:mx-auto xl:flex header-bg-dark">
+    <div class="w-full xl:mx-auto xl:w-2/6 flex flex-col justify-center items-center">
+      <h3 class="text-purple-700 text-3xl xl:text-6xl mr-10 mt-10">Quero me apresentar,</h3>
+      <p class="text-gray-300 xl:text-3xl mx-14 mt-3 mb-5">
         Meu nome é Felipe Ferreira de Barros, tenho 22 anos e atualmente estou cursando faculdade.
         Desde cedo, sempre fui fascinado pelo mundo da tecnologia e, com o tempo, essa paixão me
         levou a explorar o desenvolvimento front-end. A combinação de lógica, criatividade e a
         capacidade de criar interfaces intuitivas e visualmente agradáveis me cativou e me fez
         seguir esse caminho.</p>
 
-      <p class="text-gray-300 mx-14">
+      <p class="text-gray-300 mx-14 xl:text-3xl">
         Este portfólio foi criado com o intuito de mostrar parte das minhas habilidades
         como desenvolvedor front-end. Aqui trago alguns outros projetos que desenvolvi em HTML, CSS,
         JavaScript e frameworks modernos como Vue.js e Tailwind.</p>
+    </div>
+
+    <div class="w-full xl:w-3/6 flex mt-10 flex-col justify-center items-center">
+      <img class="w-8/12" src="../assets/images/capa.png" alt="">
     </div>
   </div>
 </template>
