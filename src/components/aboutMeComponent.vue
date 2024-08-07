faf903da4b70407f217d08862e44c9ddd159843d
FelipeBarros10 <barrosfelipe735@gmail.com>
2024-08-07 00:00:05 -0300

    Finishing the layout of projects component

--- src/components/aboutMeComponent.vue
+++ src/components/aboutMeComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full header-bg-dark xl:mx-auto xl:flex header-bg-dark">
     <div data-aos="fade-right" class="w-full xl:mx-auto xl:w-2/6 flex flex-col justify-center items-center">
-      <h3 class="text-pink-600 font-bold text-3xl xl:text-4xl mr-12 xl:mr-24 mt-10">Quero me apresentar,</h3>
+      <h3 class="text-pink-600 font-bold text-3xl xl:text-5xl mr-12 xl:mr-24 mt-10">Quero me apresentar,</h3>
       <p class="text-gray-300 xl:text-2xl mx-14 mt-3 mb-5">
         Meu nome é Felipe Ferreira de Barros, tenho 22 anos e atualmente estou cursando faculdade.
         Desde cedo, sempre fui fascinado pelo mundo da tecnologia e, com o tempo, essa paixão me
@@ -14,7 +14,6 @@
         como desenvolvedor front-end. Aqui trago alguns outros projetos que desenvolvi em HTML, CSS,
         JavaScript e frameworks modernos como Vue.js e Tailwind.</p>
     </div>
-
     <div data-aos="fade-up" class="w-full xl:w-3/6 flex mt-10 flex-col justify-center items-center">
       <img class="w-full xl:w-10/12 mb-20" src="../assets/images/capa.png" alt="">
     </div>
--- src/components/projectsComponent.vue
+++ src/components/projectsComponent.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="bg-gray-300 flex flex-col justify-center items-center">
-    <div>
-      <h1>Conheça alguns projetos feitos por mim</h1>
-      <button class="hover:bg-sky-700 transition-150 hover:text-white">Veja meu trabalho</button>
+  <div class="bg-gray-50 xl:py-20 xl:px-44  flex w-full flex-col xl:flex-row justify-center items-center">
+    <div class="w-10/12 xl:w-6/12 mt-10 xl:mt-0 py-10 xl:py-44 xl:px-32 px-14 bg-white justify-center items-center xl:items-start flex flex-col">
+      <div data-aos="fade-up" data-aos-delay="300">
+        <h1 class="font-bold text-pallete-150 text-2xl xl:text-4xl mb-5">Conheça alguns projetos feitos por mim</h1>
+        <span class="text-purple-400 font-thin text-xl xl:text-2xl ml-1 ">Projetos pessoais e profissionais..</span>
+      </div>
+      <div class="button_slide w-full xl:w-8/12">
+        <button class="text-xl">Clique aqui</button>
+        <i class="bi bi-arrow-right xl:ml-2 text-xl"></i>
+      </div>
     </div>
-    <div class="button_slide slide_right">
-      <button class="text-xl">Clique aqui</button>
-      <i class="bi bi-arrow-right xl:ml-2 text-xl"></i>
+
+    <div data-aos="fade-up" class="w-10/12 xl:w-6/12 border-t-2 xl:border-t-0 xl:border-l-2 xl:border-purple-100  border-purple-200 py-10 px-14 xl:py-44 xl:px-32 bg-white justify-center items-center xl:items-start  flex flex-col">
+      <div data-aos="fade-up" data-aos-delay="300" >
+        <h1 class="font-bold text-pallete-150 text-2xl xl:text-4xl mb-5">Conheça mais sobre a minha história</h1>
+        <span class="text-purple-400 font-thin text-xl xl:text-2xl ml-1 ">Da onde eu venho? Onde vivo? O que como?</span>
+      </div>
+      <div class="button_slide w-full xl:w-8/12">
+        <button class="text-xl">Clique aqui</button>
+        <i class="bi bi-arrow-right xl:ml-2 text-xl"></i>
+      </div>
     </div>
+
     <!-- <div class=" header-bg-dark-1 my-10 w-10/12 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
       <div class=" flex justify-center items-center">
         <span class="inline-flex items-center justify-center p-2 header-bg-dark shadow-2xl rounded-xl">
@@ -30,23 +44,30 @@ export default {
 
 <style>
 .button_slide {
-  color: #FFF;
+  color: rgba(216, 2, 134, 0.61);
   border: 2px solid rgb(216, 2, 134);
   border-radius: 0px;
-  padding: 18px 36px;
+  padding: 8px 36px;
   display: inline-block;
-  font-family: "Lucida Console", Monaco, monospace;
+  margin: 3rem 0px;
+  font-family: "Lucida Console", Monaco, bold;
   font-size: 14px;
   letter-spacing: 1px;
   cursor: pointer;
-  box-shadow: inset 0 0 0 0 #D80286;
-  -webkit-transition: ease-out 0.4s;
-  -moz-transition: ease-out 0.4s;
   transition: ease-out 0.4s;
 }
 
 .button_slide:hover {
-  box-shadow: inset 400px 0 0 0 #D80286;
+  box-shadow: inset 600px 0 0 0 #D80286;
+  transition: ease-out 0.5s;
+  color: #fff;
 }
 
+@media (min-width: 1028px){
+  .button_slide {
+    text-align: center;
+    padding: 16px 0px;
+  }
+
+}
 </style>
